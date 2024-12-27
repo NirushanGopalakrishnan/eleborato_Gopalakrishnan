a825e1ef9f388c353546bbb500aa16c75d26b05a
NirushanGopalakrishnan <nirushan.gopalakrishnan@stud.unifi.it>
2024-12-27 23:20:33 +0100

    prima prova (correzione)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
+#include "TaskManager.h"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+using namespace std;
+
+void mostraMenu() {
+    cout << "\n********************************************\n";
+    cout << "*              GESTORE TODO LIST          *\n";
+    cout << "********************************************\n";
+    cout << "1. Aggiungi Task\n";
+    cout << "2. Lista Task\n";
+    cout << "3. Segna Task come Completato\n";
+    cout << "4. Salva Task su File\n";
+    cout << "5. Carica Task da File\n";
+    cout << "6. Esci\n";
+    cout << "********************************************\n";
+    cout << "Inserisci la tua scelta: ";
 }
