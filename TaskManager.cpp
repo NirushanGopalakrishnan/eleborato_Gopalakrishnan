d81d28935f10f016878096f021b77c7727686ac6
NirushanGopalakrishnan <nirushan.gopalakrishnan@stud.unifi.it>
2024-12-27 21:34:32 +0100

    prima prova

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "TaskManager.h"
+
+
+    void aggiungiTask(const Task &task) {
+        tasks.push_back(task);
+    }
+
+    void listaTask() const {
+        if (tasks.empty()) {
+            cout << "\nNon ci sono task nella lista.\n";
+            return;
+        }
+
+        cout << "\nLista dei Task:\n";
+        for (size_t i = 0; i < tasks.size(); ++i) {
+            cout << "\n============================================\n";
+            cout << "Task " << (i + 1) << ":\n";
+            cout << tasks[i].toString();
+        }
+        cout << "\n============================================\n";
+    }
+
