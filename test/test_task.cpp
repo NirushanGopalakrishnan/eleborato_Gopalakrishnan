84a6a6344bf08f181eb7feef818f46384da15a19
NirushanGopalakrishnan <nirushan.gopalakrishnan@stud.unifi.it>
2025-01-02 21:59:57 +0100

    unit testing

--- test/test_task.cpp
+++ test/test_task.cpp
@@ -1,3 +1,36 @@
 //
 // Created by thabr on 02/01/2025.
 //
+
+//#include "lib/googletest/include/gtest/gtest.h"
+#include <gtest/gtest.h>
+#include "../Task.h"
+
+using namespace std;
+
+// Test del costruttore
+TEST(TaskTest, CostruttoreTask) {
+Task task("Titolo", "Descrizione", 3, false);
+ASSERT_EQ(task.getTitolo(), "Titolo");
+ASSERT_EQ(task.getDescrizione(), "Descrizione");
+ASSERT_EQ(task.getPriorita(), 3);
+ASSERT_FALSE(task.getCompletato());
+}
+
+// Test segna come completato
+TEST(TaskTest, SegnaComeCompletato) {
+Task task("Titolo", "Descrizione", 3, false);
+task.segnaComeCompletato();
+ASSERT_TRUE(task.getCompletato());
+}
+
+// Test serializzazione e deserializzazione
+TEST(TaskTest, SerializzazioneTask) {
+Task task("Titolo", "Descrizione", 3, true);
+string serializzato = task.serializzaPerFile();
+Task deserializzato = Task::deserializzaDaFile(serializzato);
+ASSERT_EQ(deserializzato.getTitolo(), "Titolo");
+ASSERT_EQ(deserializzato.getDescrizione(), "Descrizione");
+ASSERT_EQ(deserializzato.getPriorita(), 3);
+ASSERT_TRUE(deserializzato.getCompletato());
+}
--- test/test_taskmanager.cpp
+++ test/test_taskmanager.cpp
@@ -1,3 +1,39 @@
 //
 // Created by thabr on 02/01/2025.
 //
+
+#include <gtest/gtest.h>
+#include "../TaskManager.h"
+
+using namespace std;
+
+// Test aggiunta di un task
+TEST(TaskManagerTest, AggiungiTask) {
+    TaskManager manager;
+    manager.aggiungiTask(Task("Titolo", "Descrizione", 3, false));
+    ASSERT_EQ(manager.getTasks().size(), 1);
+    ASSERT_EQ(manager.getTasks()[0].getTitolo(), "Titolo");
+}
+
+// Test completamento di un task
+TEST(TaskManagerTest, CompletaTask) {
+    TaskManager manager;
+    manager.aggiungiTask(Task("Titolo", "Descrizione", 3, false));
+    manager.segnaTaskComeCompletato();
+    ASSERT_TRUE(manager.getTasks()[0].getCompletato());
+}
+
+// Test salvataggio e caricamento
+TEST(TaskManagerTest, SalvaCaricaTask) {
+    TaskManager manager;
+    manager.aggiungiTask(Task("Titolo", "Descrizione", 3, true));
+    manager.salvaSuFile("test_task.txt");
+
+    TaskManager managerCaricato;
+    managerCaricato.caricaDaFile("test_task.txt");
+    ASSERT_EQ(managerCaricato.getTasks().size(), 1);
+    ASSERT_EQ(managerCaricato.getTasks()[0].getTitolo(), "Titolo");
+    ASSERT_EQ(managerCaricato.getTasks()[0].getDescrizione(), "Descrizione");
+    ASSERT_EQ(managerCaricato.getTasks()[0].getPriorita(), 3);
+    ASSERT_TRUE(managerCaricato.getTasks()[0].getCompletato());
+}
